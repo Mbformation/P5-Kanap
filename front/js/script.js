ee508732d1465dc0ed23bd534a40e4262a6bee2f
Mbformation <mmvp@hotmail.fr>
2022-10-01 12:05:19 +0200

    Ajout des produits page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,28 @@
-fetch('http://localhost:3000/api/products').then(function (res) {
-    return res.json();
-}).then(function (a) {
-    console.log(a)
-})
+const data = await getData();
+
+display(data)
+
+function display(data) {
+    let html = '';
+
+    data.forEach(product => {
+        html += renderProduct(product)
+    });
+
+    document.querySelector('#items').innerHTML = html
+}
+
+function renderProduct(product) {
+    return `
+    <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`;
+}
+
+async function getData() {
+    return fetch('http://localhost:3000/api/products').then((res) => res.json())
+}
